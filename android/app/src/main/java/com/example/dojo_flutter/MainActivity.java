108a497111c69401a8edb72a3b97d281c765580d
unknown <je.mary0804@gmail.com>
2019-08-18 17:33:50 -0300

    teste

---
+++ android/app/src/main/java/com/example/dojo_flutter/MainActivity.java
@@ -0,0 +1,13 @@
+package com.dojo_flutter_sorteio_cadastro_participante;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
